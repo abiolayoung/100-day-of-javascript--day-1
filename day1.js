76bf4fe25ee91d3dcbffb60a9de0fd8806fed1af
Abeeb Adeniyi <145553438+abiolayoung@users.noreply.github.com>
2023-12-18 09:51:29 +0100

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,9 @@
+"use strict" //modern javascript code
+console.log("I am the first JS file")
+
+//variable in javascript
+//a variable is a named storage for data, we use the keyword let to declare a varible in modern javascript
+ let message = "hello world";
+
+ //single line- we can write several variables on a single line, by using to comma sign to seperate them e.g
+ let user = "Abiola", age = 25, words = "hello";
